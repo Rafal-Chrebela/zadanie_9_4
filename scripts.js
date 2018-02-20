55ec366235eea41ad9c0baf9605ecf1486771c30
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-20 20:36:10 +0100

    Modify intendations

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
-function drawTree(height) {    
+function drawTree(height) { 
+
     for(var i = 1; i < 6; i++){
-        var star = '';        
-            for(var j = 0; j < i; j++){
-                star += '*';
-            } 
+        var star = '';  
+
+        for(var j = 0; j < i; j++){
+        star += '*';
+        } 
+
         if (i === height + 1) {
             break; 
         }
+
         console.log(star); 
     }
 }
+
 drawTree(5);
