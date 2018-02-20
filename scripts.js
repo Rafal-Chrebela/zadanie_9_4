1c130867e16b476c6c7e2ed210a33901e0e7d3e7
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-20 14:34:06 +0100

    Add scripts.js with content

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function drawTree(height) {    
+    for(var i = 1; i < 6; i++){
+        var star = '';        
+            for(var j = 0; j < i; j++){
+                star += '*';
+            } 
+        if (i === height + 1) {
+            break; 
+        }
+        console.log(star); 
+    }
+}
+drawTree(5);
